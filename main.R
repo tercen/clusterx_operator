6f0e2a0404f496e469de6b91f7e4159eeb51e5d0
lucas <lucas.lelann@gmail.com>
2021-10-20 20:47:50 +0000

    add seed for the tsne option of clusterX

--- main.R
+++ main.R
@@ -3,16 +3,23 @@ library(dplyr)
 library(ClusterX)
 library(reshape2)
  
-data = (ctx = tercenCtx())  %>% 
+ctx <-tercenCtx()
+
+seed <- NULL
+if(!ctx$op.value('seed') < 0) seed <- as.integer(ctx$op.value('seed'))
+
+set.seed(seed)
+
+data <- ctx %>% 
   select(.ci, .ri, .y) %>% 
   reshape2::acast(.ci ~ .ri, value.var='.y', fill=NaN, fun.aggregate=mean) 
 
-colnames(data) = paste('c', colnames(data), sep='')
+colnames(data) <- paste('c', colnames(data), sep='')
 
-dimReduction = NULL;
-if (ctx$op.value('dimReduction') != "NULL") dimReduction = ctx$op.value('dimReduction')
+dimReduction <- NULL
+if (ctx$op.value('dimReduction') != "NULL") dimReduction <- ctx$op.value('dimReduction')
 
-dataX = ClusterX(data, dimReduction=dimReduction, outDim=as.integer(ctx$op.value('outDim')))
+dataX <- ClusterX(data, dimReduction=dimReduction, outDim=as.integer(ctx$op.value('outDim')))
 
 data.frame(.ci = seq(from=0,to=length(dataX$cluster)-1),
            cluster=paste0("cluster",dataX$cluster)) %>%
